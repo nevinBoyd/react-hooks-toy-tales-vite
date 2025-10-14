a3d343c41f0530b5bdea1c438b19c22c52cff8f0
Nevin <n.boyd1@yahoo.com>
2025-10-14 03:55:10 -0700

    Fixed prop naming to enable toy deletion

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer({ toys }) {
+function ToyContainer({ toys, onDelete }) {
   return (
     <div id="toy-collection">
       {toys.map((toy) => (
-        <ToyCard key={toy.id} toy={toy} />
+        <ToyCard
+          key={toy.id}
+          toy={toy}
+          onDelete={onDelete}
+        />
       ))}
     </div>
   );
