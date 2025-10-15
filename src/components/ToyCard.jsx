8485b514235d281995a2f52bc0ed71e216f648f5
Nevin <n.boyd1@yahoo.com>
2025-10-15 09:20:40 -0700

    Added like button and linked to backend in ToyCard

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,22 +1,34 @@
 import React from "react";
 
-function ToyCard({ toy, onDelete }) {
+function ToyCard({ toy, onDelete, onLike }) {
   const { id, name, image, likes } = toy;
 
   function handleDeleteToy() {
     onDelete(id);
   }
 
+  function handleLikeToy() {
+    const updatedLikes = likes + 1;
+
+    fetch(`http://localhost:3000/toys/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ likes: updatedLikes }),
+    })
+      .then((res) => res.json())
+      .then((updatedToy) => onLike(updatedToy));
+  }
+
   return (
     <div className="card" data-testid="toy-card">
       <h2>{name}</h2>
-      <img
-        src={image}
-        alt={name}
-        className="toy-avatar"
-      />
-      <p>{likes} Likes</p>
-      <button className="like-btn">Like {"<3"}</button>
+      <img src={image} alt={name} className="toy-avatar" />
+      <p>{likes} Likes </p>
+      <button className="like-btn" onClick={handleLikeToy}>
+        Like {"<3"}
+      </button>
       <button className="del-btn" onClick={handleDeleteToy}>
         Donate to GoodWill
       </button>
